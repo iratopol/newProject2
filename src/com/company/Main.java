0375755de42aa67a9c904146ded4d5c0b0f5651a
Ira Hulieieva <iryna.topol@gmail.com>
2020-03-08 01:01:54 +0200

    commit 8new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,5 +21,6 @@ public class Main {
         System.out.println("hiefsdgfg");
         System.out.println("hiefsdgfg");
         System.out.println("hiefsdgfg");
+        System.out.println("ira");
     }
 }
